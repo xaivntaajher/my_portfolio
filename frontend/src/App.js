92fec5e30ef37e5c25bcc9f617bd93c3bde9a372
xaivntaajher <dn_her@yahoo.com>
2023-11-19 10:24:07 -0600

    clean up to prep for start of my portifolio

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,31 +4,17 @@ import "./App.css";
 
 // Pages Imports
 import HomePage from "./pages/HomePage/HomePage";
-import LoginPage from "./pages/LoginPage/LoginPage";
-import RegisterPage from "./pages/RegisterPage/RegisterPage";
 
 // Component Imports
 import Navbar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
-
-// Util Imports
-import PrivateRoute from "./utils/PrivateRoute";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route
-          path="/"
-          element={
-            <PrivateRoute>
-              <HomePage />
-            </PrivateRoute>
-          }
-        />
-        {/* <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} /> */}
+        <Route path="/" element={<HomePage />} />
       </Routes>
       <Footer />
     </div>
@@ -36,3 +22,4 @@ function App() {
 }
 
 export default App;
+
--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,26 +1,15 @@
 import React from "react";
-import { useContext } from "react";
-import { useNavigate, Link } from "react-router-dom";
-import AuthContext from "../../context/AuthContext";
+import { Link } from "react-router-dom";
 import "./NavBar.css";
 
 const Navbar = () => {
-  const { logoutUser, user } = useContext(AuthContext);
-  const navigate = useNavigate();
   return (
     <div className="navBar">
       <ul>
         <li className="brand">
           <Link to="/" style={{ textDecoration: "none", color: "white" }}>
-            <b>React/Flask JWT</b>
+            <b>My Portfolio</b>
           </Link>
-        </li>
-        <li>
-          {user ? (
-            <button onClick={logoutUser}>Logout</button>
-          ) : (
-            <button onClick={() => navigate("/login")}>Login</button>
-          )}
         </li>
       </ul>
     </div>
@@ -28,3 +17,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
