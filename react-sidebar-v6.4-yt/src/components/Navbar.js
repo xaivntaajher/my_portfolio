29f09cce948dd4aaa4003d871b66584f3285f234
xaivntaajher <dn_her@yahoo.com>
2024-11-20 13:05:24 -0600

    create sidebar, update router

--- react-sidebar-v6.4-yt/src/components/Navbar.js
+++ react-sidebar-v6.4-yt/src/components/Navbar.js
@@ -9,11 +9,11 @@ import { IconContext } from "react-icons";
 function Navbar() {
   const [sidebar, setSidebar] = useState(false);
 
-  const showSidebar = () => setSidebar(sidebar);
+  const showSidebar = () => setSidebar(!sidebar);
 
   return (
     <>
-      <IconContext.Provider.Provider value={{ color: "undefined" }}>
+      <IconContext.Provider value={{ color: "undefined" }}>
         <div className="navbar">
           <Link to="#" className="menu-bar">
             <FaIcons.FaBars onClick={showSidebar} />
@@ -30,6 +30,7 @@ function Navbar() {
               return (
                 <li key={index} className={item.cName}>
                   <Link to={item.path}>
+                    {item.icon}
                     <span>{item.title}</span>
                   </Link>
                 </li>
@@ -37,7 +38,7 @@ function Navbar() {
             })}
           </ul>
         </nav>
-      </IconContext.Provider.Provider>
+      </IconContext.Provider>
     </>
   );
 }
--- react-sidebar-v6.4-yt/src/index.js
+++ react-sidebar-v6.4-yt/src/index.js
@@ -5,28 +5,43 @@ import {
   RouterProvider,
   Route,
   Link,
+  Outlet,
 } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
 import Projects from "./routes/Projects";
 import Contact from "./routes/Contact";
+import Navbar from "./components/Navbar";
+import "./App.css";
+
+const Applayout = () => (
+  <>
+    <Navbar />
+    <Outlet />
+  </>
+);
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "about",
-    element: <About />,
-  },
-  {
-    path: "projects",
-    element: <Projects />,
-  },
-  {
-    path: "contact",
-    element: <Contact />,
+    element: <Applayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "projects",
+        element: <Projects />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 
